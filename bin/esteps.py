49af3fa75ca86f2ec46150cdc90cb213b7553d78
Eric Moritz <ericmoritz@gmail.com>
2015-10-23 15:03:26 -0400

    lowered temp to reduce strings

---
+++ bin/esteps.py
@@ -0,0 +1,29 @@
+"""
+Usage:
+    esteps.py <esteps> <wanted> <start> <end>
+"""
+from docopt import docopt
+from decimal import Decimal as D
+
+def calc(esteps, wanted, start, end):
+    extruded = (start - end)
+    return round(D(100) / extruded * esteps)
+
+def test(expected, got):
+    assert expected == got, "Expected {expected} got {got}".format(**locals())
+
+# Make sure the function works
+test(D(928), calc(D(900), D(100), D(120), D(23)))
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='0.1')
+    esteps = arguments['<esteps>']
+    wanted = arguments['<wanted>']
+    start = arguments['<start>']
+    end = arguments['<end>']
+    print calc(
+        D(esteps),
+        D(wanted),
+        D(start),
+        D(end)
+    )
